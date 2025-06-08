8d9f8fdb8738e8fcf52f8995fb3ceb2b3f80e118
lucasalonia <salonialuca97@gmail.com>
2025-06-08 18:38:28 -0300

    Probando a ver si el deploy funciona de una vez

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,5 +1,5 @@
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 require('dotenv').config();
 const Administrativo = require('../models/Administrativo.js')
 
